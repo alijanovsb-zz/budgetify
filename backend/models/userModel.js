fa1b4d2418ad58f665ee216a8f88da9d8cb45fd3
Sardor <sardor@MacBook-Air-Sardor.local>
2022-03-22 20:34:02 -0700

    fix: added user password encryption in user chema

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -34,12 +34,23 @@ const userSchema = new mongoose.Schema(
   },
   {
     timestamps: true,
-  },
+  }
 );
 
 userSchema.methods.matchPassword = async function (password) {
   return await bcrypt.compare(password, this.password);
 };
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+  const salt = await bcrypt.genSalt(10);
+
+  this.password = bcrypt.hashSync(this.password, salt);
+
+  next();
+});
 
 const User = mongoose.model("User", userSchema);
 
